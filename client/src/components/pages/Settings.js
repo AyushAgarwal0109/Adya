e79da98ef2034c2aa8e68a0b80d1d26a804fb438
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2022-04-10 01:49:23 +0530

    bleh

--- client/src/components/pages/Settings.js
+++ client/src/components/pages/Settings.js
@@ -22,66 +22,68 @@ const Settings = () => {
       </div>
       <div className='wrapper wrapper22'>
         <h1 className='heading-settings'>Settings</h1>
-        <div className='settings-box'>
-          <form>
-            <div className='row'>
-              <div className='form-group col-sm-6'>
-                <label htmlFor='name'>Name : </label>
-                <span>
-                  <img className='updater' src={Updatelogo}></img>
-                </span>
-                <input type='text' name='namee' value={user && user.name} />
+        {user && (
+          <div className='settings-box'>
+            <form>
+              <div className='row'>
+                <div className='form-group col-sm-6'>
+                  <label htmlFor='name'>Name : </label>
+                  <span>
+                    <img className='updater' src={Updatelogo}></img>
+                  </span>
+                  <input type='text' name='namee' value={user.name} />
+                </div>
+                <div className='form-group col-sm-6'>
+                  <label htmlFor='phonenumber'>Phone Number : </label>
+                  <span>
+                    <img className='updater' src={Updatelogo}></img>
+                  </span>
+                  <input type='text' name='phone' value={user.phone} />
+                </div>
               </div>
-              <div className='form-group col-sm-6'>
-                <label htmlFor='phonenumber'>Phone Number : </label>
-                <span>
-                  <img className='updater' src={Updatelogo}></img>
-                </span>
-                <input type='text' name='phone' value={user && user.phone} />
+              <div className='row'>
+                <div className='form-group col-sm-6'>
+                  <label htmlFor='skill'>Skill : </label>
+                  <span>
+                    <img className='updater' src={Updatelogo}></img>
+                  </span>
+                  <input type='text' name='skill' value={user.skill} />
+                </div>
+                <div className='form-group col-sm-6'>
+                  <label htmlFor='group'>
+                    Individual/Associated with a SHG :{' '}
+                  </label>
+                  <span>
+                    <img className='updater' src={Updatelogo}></img>
+                  </span>
+                  <input type='text' name='group' value={user.group} />
+                </div>
               </div>
-            </div>
-            <div className='row'>
-              <div className='form-group col-sm-6'>
-                <label htmlFor='skill'>Skill : </label>
-                <span>
-                  <img className='updater' src={Updatelogo}></img>
-                </span>
-                <input type='text' name='skill' value={user && user.skill} />
+              <div className='row'>
+                <div className='form-group col-sm-6'>
+                  <label htmlFor='pin'>PIN : </label>
+                  <span>
+                    <img className='updater' src={Updatelogo}></img>
+                  </span>
+                  <input type='password' name='password' value={'****'} />
+                </div>
               </div>
-              <div className='form-group col-sm-6'>
-                <label htmlFor='group'>
-                  Individual/Associated with a SHG :{' '}
-                </label>
-                <span>
-                  <img className='updater' src={Updatelogo}></img>
-                </span>
-                <input type='text' name='group' value={user && user.group} />
-              </div>
-            </div>
-            <div className='row'>
-              <div className='form-group col-sm-6'>
-                <label htmlFor='pin'>PIN : </label>
-                <span>
-                  <img className='updater' src={Updatelogo}></img>
-                </span>
-                <input type='password' name='password' value={'****'} />
-              </div>
-            </div>
-            <input type='submit' value='Update' className='btn-sbmt' />
-            <ul class='bg-bubbles'>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </form>
-        </div>
+              <input type='submit' value='Update' className='btn-sbmt' />
+              <ul class='bg-bubbles'>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
