0f37ad5d57ef4e10b6306c4ddc8964ff473d0eea
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-04-28 01:25:37 +0530

    aaaaaaaaaaaaacc

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ const App = () => {
                   <Schemes />
                   <Donate />
                   { /* <News /> */}
-                  {/* <Testimonials /> */}
+                  <Testimonials />
                   <Contact /> 
 
                 </Route>
--- client/src/components/layout/Testimonials.js
+++ client/src/components/layout/Testimonials.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import image from "../../assets/images/testi.png";
 import "../../assets/css/testimonials.css";
+import TestimonialCard from './TestimonialCard';
+
 function Testimonials() {
   return (
     <div>
         <center><div className="testimonial-section">
 
-        <img src={image} alt="img" className="testimonial-img" />
-        <img src={image} alt="img" className="testimonial-img" />
-        <img src={image} alt="img" className="testimonial-img" />
-        <img src={image} alt="img" className="testimonial-img" />
+        <div className="testimonial-img"><TestimonialCard /></div>
+        <div className="testimonial-img"><TestimonialCard /></div>
+        <div className="testimonial-img"><TestimonialCard /></div>
+        <div className="testimonial-img"><TestimonialCard /></div>
 
         </div>
         </center>
